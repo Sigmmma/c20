5f7a6a199363a7672354c808789760ebf66a2663
csauve <csauve@t3hz0r.com>
2020-05-03 13:50:53 -0700

    Improve UX of search results

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,30 +1,66 @@
 const {html, render, Component} = htmPreact;
 
-const MAX_SEARCH_SUGGESTIONS = 1;
-
 class Search extends Component {
   constructor() {
     super();
-    this.state = {disabled: true, searchResults: [], suggestions: []};
     this.handleChange = this.handleChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.state = {
+      disabled: true,
+      query: "",
+      searchResults: [],
+      selectedResultIndex: -1
+    };
   }
-  handleChange(e) {
-    if (!this.state.disabled) {
-      const query = e.target.value;
-      const searchResults = this.searchIndex.search(query);
-      const suggestions = new Set();
-      this.searchIndex.autoSuggest(query).forEach(s => {
-        if (suggestions.size < MAX_SEARCH_SUGGESTIONS) {
-          suggestions.add(s.terms[0]);
-        }
-      });
+
+  handleKeyDown(e) {
+    if (e.key == "Escape") {
       this.setState({
+        query: "",
+        searchResults: [],
+        selectedResultIndex: -1
+      });
+      if (this.inputRef) {
+        this.inputRef.blur();
+      }
+    } else if (e.key == "ArrowDown") {
+      this.setState({selectedResultIndex: Math.min(
+        this.state.selectedResultIndex + 1,
+        this.state.searchResults.length - 1
+      )});
+    } else if (e.key == "ArrowUp") {
+      this.setState({selectedResultIndex: Math.max(
+        this.state.selectedResultIndex - 1,
+        -1
+      )});
+    } else if (e.key == "Enter") {
+      if (this.state.selectedResultIndex != -1) {
+        window.location = this.state.searchResults[this.state.selectedResultIndex].id;
+      }
+    }
+  }
+
+  handleChange(query) {
+    if (!this.state.disabled) {
+      let searchResults = this.searchIndex.search(query);
+
+      //if no results, try suggested search instead
+      if (searchResults.length == 0) {
+        const suggestions = this.searchIndex.autoSuggest(query);
+        if (suggestions.length > 0) {
+          const firstSuggestionTerm = suggestions[0].terms[0];
+          searchResults = this.searchIndex.search(firstSuggestionTerm);
+        }
+      }
+
+      this.setState({
+        query,
         searchResults,
-        suggestions: [...suggestions],
-        nonempty: !!query
+        selectedResultIndex: -1
       });
     }
   }
+
   componentDidMount() {
     fetch("/assets/search-index.json")
       .then(res => res.text())
@@ -40,31 +76,41 @@ class Search extends Component {
         });
         this.setState({disabled: false});
       });
+    window.addEventListener("keydown", (e) => {
+      if (e.key == "s" && this.inputRef && document.activeElement !== this.inputRef) {
+        this.inputRef.focus();
+        e.preventDefault();
+      }
+    });
   }
+
   render() {
     return html`
       <input
-        class="search-input ${this.state.nonempty ? "nonempty" : ""}"
+        ref=${(el) => this.inputRef = el}
+        class="search-input ${!!this.state.query ? "nonempty" : ""}"
         type="text"
-        list="c20-search-suggestions"
         placeholder="Search c20..."
         disabled=${this.state.disabled}
-        onInput=${this.handleChange}
+        value=${this.state.query}
+        onInput=${(e) => this.handleChange(e.target.value)}
+        onKeyDown=${this.handleKeyDown}
       />
-      <datalist id="c20-search-suggestions">
-        ${this.state.suggestions.map(suggestion => html`
-          <option value=${suggestion}/>
-        `)}
-      </datalist>
-      ${this.state.searchResults.length > 0 && html`
-        <div class="search-results">
+      ${this.state.query != "" && html`
+        <nav class="search-results">
+          <button class="clear-button" onClick=${() => this.handleChange("")}>Close</button>
           <h1>Search results</h1>
-          <ul>
-            ${this.state.searchResults.map(result =>
-              html`<li><a href=${result.id}>${result.title}</a></li>`
-            )}
-          </ul>
-        </div>
+          ${this.state.searchResults.length > 0 ? html`
+            <ul>
+              ${this.state.searchResults.map((result, i) => {
+                const isSelected = i == this.state.selectedResultIndex;
+                return html`<li class=${isSelected ? "selected" : ""}><a href=${result.id}>${result.title}</a></li>`
+              })}
+            </ul>
+          ` : html`
+            <p>No results found for <strong>${this.state.query}</strong></p>
+          `}
+        </nav>
       `}
     `;
   }
