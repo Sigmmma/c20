73cb08ca4ebffcc54ef456f622c4322d2a8c776c
csauve <csauve@t3hz0r.com>
2020-05-31 15:37:40 -0700

    Add source and edit links to top of the article

--- src/templates/shared/footer.js
+++ src/templates/shared/footer.js
@@ -3,7 +3,6 @@ const {html, REPO_URL} = require("./bits");
 const LICENSE_URL = "https://creativecommons.org/licenses/by-sa/3.0/";
 
 const footer = (page, metaIndex) => {
-  const srcUrl = `${REPO_URL}/tree/master/src/content${page._path}`;
   return html`
     <footer class="content-footer">
       <p>
@@ -13,8 +12,6 @@ const footer = (page, metaIndex) => {
           ${metaIndex.packageVersion && html`
             <a href="${REPO_URL}">c20 v${metaIndex.packageVersion}</a>
           `}
-          •
-          <a href="${srcUrl}">Source</a>
           •
           <a href="#">Go to top</a>
         </small>
