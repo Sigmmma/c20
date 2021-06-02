3aca73d0cf9f68c898b964790d373e8a2e422879
num0005 <double.null@outlook.com>
2021-06-03 00:27:29 +0100

    Include "H1A-EK" in the abbreviation list.

--- src/build/render/components/markdown/abbreviations.js
+++ src/build/render/components/markdown/abbreviations.js
@@ -1,14 +1,17 @@
 module.exports = Object.entries({
-  H1X: {
+  "H1X": {
     en: "Halo 1 for Xbox (Bungie)"
   },
-  H1PC: {
+  "H1PC": {
     en: "Halo 1 PC retail for Windows and Mac (Gearbox)"
   },
-  H1CE: {
+  "H1CE": {
     en: "Halo Custom Edition (Gearbox)"
   },
-  H1A: {
+  "H1A-EK": {
+    en: "Halo 1 Anniversary Editing Kit (343i)"
+  },
+  "H1A": {
     en: "Halo 1 Anniversary for MCC, aka. CEA (343i)"
   }
 });
